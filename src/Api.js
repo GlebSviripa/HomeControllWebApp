46e325b0434194baafd9532d5a6336fabd354a94
Gleb Sviripa <gsviripa@tula.co>
2020-10-30 07:35:39 +0200

    fix for fetch

--- src/Api.js
+++ src/Api.js
@@ -4,14 +4,14 @@ class Api {
     static async updateRGB(RGB, ip) {
         console.log(ip)
         const response =
-            await fetch(ip + "/led?R=" + RGB.R + "&G=" + RGB.G + "&B=" + RGB.B, {mode: 'no-cors'})
+            await fetch("http://" + ip + "/led?R=" + RGB.R + "&G=" + RGB.G + "&B=" + RGB.B, {mode: 'no-cors'})
         console.log(await response.text())
     }
 
     static async updateMode(mode, ip) {
         console.log(ip)
         const response =
-            await fetch(ip + "/ledMode?mode=" + mode.toString(), {mode: 'no-cors'})
+            await fetch("http://" + ip + "/ledMode?mode=" + mode.toString(), {mode: 'no-cors'})
         console.log(await response.text())
     }
 }
